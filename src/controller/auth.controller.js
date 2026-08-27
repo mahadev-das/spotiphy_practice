2dff63541b3910220486ce0ce6d9b2684516a25a
mahadev_das <mahadev.das@cordiia.in>
2026-08-27 17:45:38 +0530

    password hashing implement, and etc...

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,14 +1,28 @@
 const usermodel = require("../models/auth.model");
+const bcrypt=require('bcryptjs')
 const jwt = require("jsonwebtoken");
 
 async function userRagistration(req, res) {
   const { username, email, role, password } = req.body;
 
+  const isUserExist= await usermodel.findOne({
+    $or:[{username},{email}]
+  })
+
+  if(isUserExist){
+    return res.status(409).json({
+      message:'user alrady exist',
+      isUserExist
+    })
+  }
+
+  const hash=await bcrypt.hash(password,10);
+
   const user = await usermodel.create({
     username,
     email,
     role,
-    password,
+    password:hash,
   });
 
   const token = jwt.sign({ userid: user._id }, process.env.JWT_SECRETE);
@@ -29,12 +43,40 @@ async function userLogin(req,res){
     console.log(req.body,'userLogin');
     const {username,email,password}=req.body;
 
-    const user= usermodel.find({
-
+    const user=await usermodel.find({
+      $or:[{username},{email}]
     })
 
+    console.log(user,'user');
+    
+
+    if(user.length<=0){
+      return res.status(401).json({
+        message:'Unauthorize user'
+      })
+    }
+
+    const passwordCheck= await usermodel.find({
+      password
+    })
+
+    console.log(passwordCheck,'passwordcheck');
+    
+    if(passwordCheck.length<=0){
+      return res.status(401).json({
+        message:'Unauthorized user , wron password'
+      })
+    }
+
+    const token =jwt.sign({userid:user._id},process.env.JWT_SECRETE);
+    res.cookie('token',token);
     res.status(200).json({
-        success:'true'
+        success:'true',
+        message:'login successfully',
+        user,
+        username,
+        email,
+        password
     })
 }
 
